5fb8d5bdf9ce89c215760172e4664d4eec1e5d41
Коноваленко Даніїл <115369675+NureKonovalenkoDaniil@users.noreply.github.com>
2025-06-07 19:25:18 +0300

    Delete Task2/arkpz-pzpi-22-2-konovalenko-daniil-lab2 directory

--- Task2/arkpz-pzpi-22-2-konovalenko-daniil-lab2/MedicationManagement/Models/ReplenishmentRecommendation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MedicationManagement.Models
-{
-    public class ReplenishmentRecommendation
-    {
-        public int MedicineId { get; set; }
-        public string MedicineName { get; set; }
-        public int RecommendedQuantity { get; set; }
-    }
-}
